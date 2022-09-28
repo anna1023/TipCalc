77f0a12e67831745352f15efd572566eb93153c6
anna1023 <annac101@nycstudents.net>
2022-09-28 09:24:35 -0400

    1

--- src/TipsCalc.java
+++ src/TipsCalc.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class TipsCalc {
+import java.util.Scanner;
+public class TipsCalc {
+    public static void main (String[] args)
+    {
+        Scanner scan=new Scanner (System.in);
+        System.out.println("what is your bill?");
+        double bill= scan.nextDouble();
+        System.out.println("How much would you like to tip?");
+        double tip=scan.nextDouble();
+        System.out.println("How many people do you have?");
+        int people=scan.nextInt();
+        double tiptotal=(tip/100)*bill;
+        double tipperson=tiptotal/people;
+        double total=bill+tiptotal;
+        double totalperson=total/people;
+        System.out.println("Total: "+String.format("%.2f", total));
+        System.out.println("Tip: "+String.format("%.2f",tiptotal));
+        System.out.println("Here is your total per person: "+String.format("%.2f",totalperson));
+        System.out.println("Here is your tips per person: "+String.format("%.2f",tipperson));
+
+    }
 }
